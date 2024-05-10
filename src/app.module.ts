708d4b7881b2b26a4e220e85c70bdf575a066a5b
Tiago Persch <thiagopersch@gmail.com>
2024-05-10 00:31:28 -0400

    add new modifications

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { PrismaClient } from '@prisma/client';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MonthsModule } from './months/months.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { SessionModule } from './session/session.module';
+import { UsersModule } from './users/users.module';
+import { YearModule } from './year/year.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    YearModule,
+    MonthsModule,
+    PrismaClient,
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    AuthModule,
+    SessionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
